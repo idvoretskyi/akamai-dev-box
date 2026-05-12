5f44ed61c9e0cdeccdd5c826b54fd4c5ee07c972
Ihor Dvoretskyi <ihor@linux.com>
2026-05-12 22:59:59 +0300

    chore: update git_ref to c6a15d6 (hostname /etc/hosts fix)
    Signed-off-by: Ihor Dvoretskyi <ihor@dvoretskyi.com>

--- tofu/main.tf
+++ tofu/main.tf
@@ -25,7 +25,7 @@ locals {
   tunnel_name = var.vscode_tunnel_name == "" ? local.hostname : var.vscode_tunnel_name
   # Git ref used to fetch scripts from GitHub at boot time.
   # Uses the current HEAD commit SHA for a stable, pinned reference.
-  git_ref = "65d73b8c229cc12f92258baae7805c1fcef7c7b0"
+  git_ref = "c6a15d636852f71e09f9636546b67c5f48fa041e"
 
   cloud_init = templatefile("${path.module}/cloud-init/main.yaml.tpl", {
     username           = local.username
