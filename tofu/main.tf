0464a0c7de3e48758460ba67ab3371665b5e5fbb
Ihor Dvoretskyi <ihor@linux.com>
2026-05-12 23:24:57 +0300

    tofu: bump git_ref to 5156ba5 (clear MOTD)

--- tofu/main.tf
+++ tofu/main.tf
@@ -25,7 +25,7 @@ locals {
   tunnel_name = var.vscode_tunnel_name == "" ? local.hostname : var.vscode_tunnel_name
   # Git ref used to fetch scripts from GitHub at boot time.
   # Uses the current HEAD commit SHA for a stable, pinned reference.
-  git_ref = "6830d739da37e645f6b2ade38bb663c390665c25"
+  git_ref = "5156ba51660e0bbf265e9daace1b3ef055ff4ce3"
 
   cloud_init = templatefile("${path.module}/cloud-init/main.yaml.tpl", {
     username           = local.username
